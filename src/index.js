d0897e369150cbef66b26ff3830538bd297e1cd6
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2022-12-01 23:52:59 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import 'normalize.css';
+import './index.css';
+import App from './App';
+
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+
+    <App tab='home' />
+
+);
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,10 @@
+import logo from "../assets/images/logo.svg"
+import main from "../assets/images/main.svg"
+
+const Landing = () => {
+  return (
+    <h1>Landing</h1>
+  )
+}
+
+export default Landing
